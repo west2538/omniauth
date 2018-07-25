8f5b4a79938586975698540d2a98476f8cd1efb3
Haruhisa Nishimura <west2538@gmail.com>
2018-07-25 22:53:40 +0900

    csslinktag2

--- lib/omniauth/form.rb
+++ lib/omniauth/form.rb
@@ -93,14 +93,14 @@ module OmniAuth
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
         <title>#{title}</title>
         #{css}
         #{header_info}
       </head>
       <body>
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
       <div class="mt-3" style="width: 320px; margin: auto;">
 
