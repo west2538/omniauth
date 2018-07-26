9ca3ddbd2faa792ea1256ff006b74a4dd6633b8a
Haruhisa Nishimura <west2538@gmail.com>
2018-07-26 09:24:37 +0900

    csslinktag4

--- lib/omniauth/form.rb
+++ lib/omniauth/form.rb
@@ -96,6 +96,17 @@ module OmniAuth
         <title>#{title}</title>
         #{css}
         #{header_info}
+
+        <script>
+        $(document).on "page:change", ->
+            window.prevPageYOffset = window.pageYOffset
+            window.prevPageXOffset = window.pageXOffset
+          $(document).on "page:load", ->
+            if $(".fix-scroll").length > 0
+              $('.fix-scroll').hide().show() # force re-render -- having an issue with that on Chrome/OSX
+              window.scrollTo window.prevPageXOffset, window.prevPageYOffset
+        </script>
+
       </head>
       <body>
 
@@ -104,9 +115,9 @@ module OmniAuth
 
       <div class="mt-3" style="width: 320px; margin: auto;">
 
-      <div class="container">
-
       <div class="jumbotron">
+
+      <div class="container">
 
         <h2>ルートを探せ</h2>
 
@@ -149,7 +160,6 @@ module OmniAuth
 
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
       </body>
       </html>
       HTML
